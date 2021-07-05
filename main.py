95d5a87e49cd2661fff3bd188f747a3433b89e8d
ebhose <ebhose@localdomain.local>
2021-07-05 14:57:27 +0200

    Move function is_prime to file prime.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
-import math
 
-
-def is_prime(num):
-
-    for divisor in range(2, int(math.sqrt(num) + 1)):
-        if num % divisor == 0:
-            return False
-    return True
+from prime import is_prime
 
 
 print(is_prime(73))
+print(is_prime(37))
+print(is_prime(70))
