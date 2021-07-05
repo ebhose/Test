8f79378d47d01771fa5ef34c9e647979a332b2d9
ebhose <ebhose@localdomain.local>
2021-07-05 14:18:50 +0200

    Add function is_prime() to file main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import math
+
+
+def is_prime(num):
+
+    for divisor in range(2, int(math.sqrt(num) + 1)):
+        if num % divisor == 0:
+            return False
+    return True
+
+
+print(is_prime(73))
